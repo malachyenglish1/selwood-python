f647a778c9f7204493be0ca70f4fcb2137701abf
Malachy English <malachyenglish@gmail.com>
2019-01-15 14:04:08 +0000

    added date manipulation to trade-tracker. Isolated TradeId

--- projects/new-trade-tracker.py
+++ projects/new-trade-tracker.py
@@ -1,8 +1,20 @@
 #import relevant packages
 
 import numpy as np
+import pandas as pd
 import glob
 import os
+
+#Some display options for the terminal relevant to pandas
+pd.options.display.max_rows = 999
+pd.options.display.max_columns = 999
+
+from datetime import datetime
+from dateutil.relativedelta import relativedelta, FR
+
+#This gives the last Fri before today
+#Can use this to pull the most relevant report and to check the differences in trades
+datetime.now() + relativedelta(weekday=FR(-1))
 
 #find most recent folder from directory
 list_of_folders = glob.glob('Z:/QuantifiReport/*')
@@ -11,11 +23,34 @@ latest_folder = max(list_of_folders, key=os.path.getctime)
 print(latest_folder)
 
 #import the relevant csv using context manager
-trade_file = str(latest_folder) + "\\" + 'LCS_DetailedReport.csv'
+datail_file = str(latest_folder) + "\\" + 'LCS_DetailedReport.csv'
+print(datail_file)
+
+trade_file = str(latest_folder) + "\\" + 'LCS_TradeReport.csv'
 print(trade_file)
 
+#Check the first five lines of the file and the header
+#Use pandas to import the csv as a dataframe within a context manager
+with open(datail_file, 'r') as f:
+    df = pd.read_csv(f)
+    print(df.head())
+    print(list(df.columns.values))
+    #remove duplicate TradeId's
+    df = df.drop_duplicates(subset=["TradeId"], keep= 'first')
+    df.set_index("TradeId", inplace = True)
+    print(df.loc[:, ["Description"]].head())
+    print("Total number of trades: " + str(len(df.index)))
+    print(df.shape)
+
+
 '''
+#Check the top 3 lines of the file
 with open(trade_file, 'r') as file:
-    head = [next(trade_file) for x in range (10)]
+    print(file.readline())
+    print(file.readline())
+    print(file.readline())
+    head = [next(file) for x in range (3)]
     print(head)
 '''
+
+#
--- scripts/ticker-changes.py
+++
@@ -1,8 +0,0 @@
-#Pull reference data from markit files
-
-import xml.etree.ElementTree as ET
-
-with open('Z:\MarkitData\20190108\RED_ENTITY_20190108.xml', 'r') as file:
-    tree = ET.parse(file)
-    root = tree.getroot()
-    
